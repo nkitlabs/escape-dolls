fb0cc8dc0dceea9b1e4f6cf1fe41517153b5eb88
nkitlabs <natthakunk0109@gmail.com>
2023-01-06 00:50:37 +0700

    register functionMapping item01Modal

--- src/hooks/useGameSetup.ts
+++ src/hooks/useGameSetup.ts
@@ -2,12 +2,18 @@ import { useEffect, useState } from 'react'
 
 import { dataLoaderService } from 'services/dataLoaderService'
 
+import { gameStore } from 'stores/gameStore'
+
+import { openItem01Modal } from 'views/core/CoreEscapeGame/Item01Modal'
+
 export const useGameSetup = () => {
   const [isReady, setIsReady] = useState(false)
 
   useEffect(() => {
+    if (isReady) return
     const initTask = async () => {
       await dataLoaderService.setStoryMapping()
+      gameStore.registerFunctionsMapping(1, openItem01Modal)
     }
 
     initTask().then(() => {
--- src/views/core/CoreEscapeGame/Item01Modal/index.tsx
+++ src/views/core/CoreEscapeGame/Item01Modal/index.tsx
@@ -1,11 +1,44 @@
-import { Stack, Typography } from '@mui/material'
+import { Button, Typography } from '@mui/material'
+import { Stack } from '@mui/system'
+import { observer } from 'mobx-react-lite'
 
-export const Item01Modal = () => {
-  return (
-    <Stack>
-      <Typography variant="h6" fontWeight="bold">
-        You observe: A safe
-      </Typography>
-    </Stack>
-  )
+import { useBreakpoints } from 'hooks/useBreakpoints'
+
+import { dialogStore } from 'stores/dialogStore'
+import { gameStore } from 'stores/gameStore'
+
+import { Header, ModalWrapper } from './components'
+
+type Item01ModalProps = {
+  name: string
+  img: string
+  description: string
 }
+
+export const openItem01Modal = (params: string[]) => {
+  dialogStore.open(() => <Item01Modal name={params[0]} img={params[1]} description={params[2]} />, {
+    hideHeader: true,
+    borderRadius: 16,
+  })
+}
+
+export const Item01Modal = observer(({ name, img, description }: Item01ModalProps) => {
+  const { downSm } = useBreakpoints()
+  const imgUrl = gameStore.imgRecord[img]
+
+  return (
+    <ModalWrapper>
+      <Stack alignItems="center">
+        <Header>{`You observe`}</Header>
+        <Header>{name}</Header>
+      </Stack>
+      <img src={imgUrl} height={downSm ? 120 : 240} />
+      <Typography align="center" color="primary.darken" variant={downSm ? 'body2' : 'body1'}>
+        {description}
+      </Typography>
+      <Button sx={{ height: 40, width: 160 }} onClick={() => dialogStore.close()}>
+        Back
+      </Button>
+    </ModalWrapper>
+  )
+})
